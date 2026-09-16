59aa741336f62fdb7694c5b48bba6341e9eef9c7
LK0305 <2024120902014@std.uestc.edu.cn>
2026-09-16 17:15:37 +0800

    chore:清理学习笔记，仅保留核心API项目文件

--- 4.py
+++
@@ -1,9 +0,0 @@
-# import random
-# teacher = ['a','b','c','d','e','f','g','h']
-# offices = [[],[],[]]
-# for name in teacher:
-#     num = random.randint(0,2)
-#     offices[num].append(name)
-# i=1.txt
-# for office in offices:
-#     print(office)
